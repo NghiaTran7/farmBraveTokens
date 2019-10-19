88660d8a30a476b5978c5bb52e44d34618abe1c6
Nghia <nghia-tran@uiowa.edu>
2019-10-18 22:47:12 -0500

    tested and worked!!!

--- autoMove.py
+++ autoMove.py
@@ -25,24 +25,25 @@ def autoMove():
     #hoovers mouse over certain parameters for pixels, and then clicks on it.
     #4 minute intervals
     pyautogui.moveTo(3485, 1934, duration=0.25)
-    #pyautogui.click(button='left')
+    pyautogui.click(button='left')
     print('WE are hard at work, farming for you! Sit back, relax, and watch the BAT coins roll in!!!!')
     start = time.time()
-    time.sleep(15)
+    #time.sleep(1)
     end = time.time()
     ending = end-start
     ending2 = str(round(ending, 2))
-    print ('Duration of script: ' + str(ending2) + " seconds")
+    #print ('Duration of script: ' + str(ending2) + " seconds")
 
 #function to refresh amazon browser every 2 minutes 30 seconds
 def autoRefresh():
     pyautogui.typewrite(['f5'])
     print ('We refreshed your browser to trick them into giving us more ads!! MUAHAHAHAHA!!!!')
-    time.sleep(15)
+    time.sleep(30)
 
 def autoClick():
     pyautogui.click(button='left')
     print ('We clicked left!')
+    time.sleep(3)
 
 
 #schedules to run the automove
@@ -65,3 +66,4 @@ if __name__ == '__main__':
     #printPosition()
     #autoMove()
     timeBot()
+    pyautogui.FAILSAFE = False
